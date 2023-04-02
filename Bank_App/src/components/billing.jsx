05e6319ed73eb50c98e7e2ba650ffafa64018d4c
vasanthvz <vasanthv1373@gmail.com>
2023-04-02 14:22:54 +0530

    modified:   .DS_Store 	deleted:    Bank_App/.gitignore 	deleted:    Bank_App/index.html 	deleted:    Bank_App/package-lock.json 	deleted:    Bank_App/package.json 	deleted:    Bank_App/public/vite.svg 	deleted:    Bank_App/src/App.jsx 	deleted:    Bank_App/src/assets/Discount.svg 	deleted:    Bank_App/src/assets/Send.svg 	deleted:    Bank_App/src/assets/Shield.svg 	deleted:    Bank_App/src/assets/Star.svg 	deleted:    Bank_App/src/assets/airbnb.png 	deleted:    Bank_App/src/assets/apple.svg 	deleted:    Bank_App/src/assets/arrow-up.svg 	deleted:    Bank_App/src/assets/bill.png 	deleted:    Bank_App/src/assets/binance.png 	deleted:    Bank_App/src/assets/card.png 	deleted:    Bank_App/src/assets/close.svg 	deleted:    Bank_App/src/assets/coinbase.png 	deleted:    Bank_App/src/assets/dropbox.png 	deleted:    Bank_App/src/assets/facebook.svg 	deleted:    Bank_App/src/assets/google.svg 	deleted:    Bank_App/src/assets/index.js 	deleted:    Bank_App/src/assets/instagram.svg 	deleted:    Bank_App/src/assets/linkedin.svg 	deleted:    Bank_App/src/assets/logo.svg 	deleted:    Bank_App/src/assets/menu.svg 	deleted:    Bank_App/src/assets/people01.png 	deleted:    Bank_App/src/assets/people02.png 	deleted:    Bank_App/src/assets/people03.png 	deleted:    Bank_App/src/assets/quotes.svg 	deleted:    Bank_App/src/assets/robot.png 	deleted:    Bank_App/src/assets/twitter.svg 	deleted:    Bank_App/src/components/Bussiness.jsx 	deleted:    Bank_App/src/components/CTA.jsx 	deleted:    Bank_App/src/components/Hero.jsx 	deleted:    Bank_App/src/components/Navbar.jsx 	deleted:    Bank_App/src/components/billing.jsx 	deleted:    Bank_App/src/components/button.jsx 	deleted:    Bank_App/src/components/carddeal.jsx 	deleted:    Bank_App/src/components/client.jsx 	deleted:    Bank_App/src/components/feedbackcard.jsx 	deleted:    Bank_App/src/components/footer.jsx 	deleted:    Bank_App/src/components/getStarted.jsx 	deleted:    Bank_App/src/components/index.js 	deleted:    Bank_App/src/components/stats.jsx 	deleted:    Bank_App/src/components/testimonials.jsx 	deleted:    Bank_App/src/constants/index.js 	deleted:    Bank_App/src/index.css 	deleted:    Bank_App/src/main.jsx 	deleted:    Bank_App/src/style.js 	deleted:    Bank_App/tailwind.config.js 	deleted:    Bank_App/vite.config.js

--- Bank_App/src/components/billing.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Billing = () => {
-  return (
-    <div>billing</div>
-  )
-}
-
-export default Billing
--- Bank_App/src/components/feedbackcard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Feedbackcard = () => {
-  return (
-    <div>Feedbackcard</div>
-  )
-}
-
-export default Feedbackcard
