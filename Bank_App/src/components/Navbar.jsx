f7eddc2ec9366a802df9a7bd99e360dfa34354f2
vasanthvz <vasanthv1373@gmail.com>
2023-04-03 21:05:05 +0530

    modified:   Bank_App/src/components/Navbar.jsx 	modified:   Bank_App/src/index.css

--- Bank_App/src/components/Navbar.jsx
+++ Bank_App/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import{useState} from 'react'
+import '../index.css'
 import{close,logo,menu} from '../assets'
 import{navLinks} from '../constants'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center'>
+      
+    </nav>
   )
 }
 
