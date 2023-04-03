62e2dac4fca7886bb4c2392d5636fbae8f7ed0f4
vasanthvz <vasanthv1373@gmail.com>
2023-04-03 21:32:06 +0530

    modified:   Bank_App/src/components/Navbar.jsx

--- Bank_App/src/components/Navbar.jsx
+++ Bank_App/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import{close,logo,menu} from '../assets'
 import{navLinks} from '../constants'
 const Navbar = () => {
   return (
-    <nav className='w-full flex py-6 justify-between items-center'>
-      
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="hoobank" className="w-124px"/>
     </nav>
   )
 }
