f4260ed2ef4e01fb9532b3261244ae799bba3145
vasanthvz <vasanthv1373@gmail.com>
2023-04-04 14:36:36 +0530

    modified:   Bank_App/src/components/index.js

--- Bank_App/src/components/index.js
+++ Bank_App/src/components/index.js
@@ -1,28 +1,23 @@
+import Navbar from "./Navbar";
 import Billing from "./Billing";
-import Business from "./Business";
-import Button from "./Button";
 import CardDeal from "./CardDeal";
+import Business from "./Business";
 import Clients from "./Clients";
 import CTA from "./CTA";
-import FeedBackCard from "./FeedBackCard";
-import Footer from "./Footer";
-import Hero from "./Hero";
-import Navbar from "./Navbar";
 import Stats from "./Stats";
+import Footer from "./Footer";
 import Testimonials from "./Testimonials";
+import Hero from "./Hero";
 
-export{
-    Navbar,
-    Billing,
-    CardDeal,
-    Business,
-    Clients,
-    CTA,
-    Stats,
-    Footer,
-    FeedBackCard,
-    Testimonials,
-    Hero,
-    Button,
-    GetStarted
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
 };
